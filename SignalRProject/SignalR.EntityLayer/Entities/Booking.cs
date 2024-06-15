90a242f9c583a840d193e62243eef384c1abe445
Sena Ekincioğlu <ekincioglusena1@gmail.com>
2024-06-15 10:12:07 +0300

    AspNet Core 6.0 Api - SignalR
    AspNet Core 6.0 Api - A Dynamic QR Code Order Management for Restaurants with SignalR

---
+++ SignalRProject/SignalR.EntityLayer/Entities/Booking.cs
@@ -0,0 +1,11 @@
+﻿namespace SignalR.EntityLayer.Entities
+{
+    public class Booking // Rezervasyon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Mail { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
