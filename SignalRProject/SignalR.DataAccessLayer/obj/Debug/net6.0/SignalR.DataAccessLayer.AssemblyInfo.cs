d3d392faf371028bf486341171d9a205ccf20a43
Sena Ekincioğlu <ekincioglusena1@gmail.com>
2024-06-15 10:36:58 +0300

    Migrations oluşturuldu

--- SignalRProject/SignalR.DataAccessLayer/obj/Debug/net6.0/SignalR.DataAccessLayer.AssemblyInfo.cs
+++ SignalRProject/SignalR.DataAccessLayer/obj/Debug/net6.0/SignalR.DataAccessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignalR.DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90a242f9c583a840d193e62243eef384c1abe445")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8850c09f0f8c386f8c212ad58a347a225bc2ddaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignalR.DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignalR.DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
