d1ae33a20a604ecee0b355f26ac45cb64dbe8bad
Sena Ekincioğlu <ekincioglusena1@gmail.com>
2024-07-11 12:33:36 +0300

    Dto katmanına ait tüm entity ler için Crud Dto ları oluşturuldu.

---
+++ SignalRProject/SignalR.DtoLayer/TestimonialDto/CreateTestimonialDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DtoLayer.TestimonialDto
+{
+    public class CreateTestimonialDto
+    {
+        //Id olmaz çünkü Id otomatik artan.
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Comment { get; set; } //yorum
+        public string ImageUrl { get; set; }
+        public bool Status { get; set; }
+    }
+}
