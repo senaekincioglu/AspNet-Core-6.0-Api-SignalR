09e964738daf0731d0c6f69e7df2d050a111d228
Sena Ekincioğlu <ekincioglusena1@gmail.com>
2024-07-11 13:06:15 +0300

    AutoMapper paketi kurularak ve Api katmanına Dto katmanı referans verilerek Entity lerin Dto ya dönüşmesi veya Dto nun Entity e dönüşmesi için her entity için Mapping işlemleri yapıldı.

---
+++ SignalRProject/SignalRApi/Mapping/CategoryMapping.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore.Query;
+using SignalR.DtoLayer.CategoryDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Mapping
+{
+    public class CategoryMapping:Profile
+    {
+        public CategoryMapping()
+        {
+            CreateMap   <Category, ResultCategoryDto>().ReverseMap ();
+            CreateMap<Category, UpdateCategoryDto>().ReverseMap();
+            CreateMap<Category, GetCategoryDto>().ReverseMap();
+            CreateMap<Category, CreateCategoryDto>().ReverseMap();
+
+        }
+    }
+}
