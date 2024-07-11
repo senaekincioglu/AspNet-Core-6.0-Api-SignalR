96c12b201120ae9fae0017865e8a3ccc8be19859
Sena Ekincioğlu <ekincioglusena1@gmail.com>
2024-07-11 10:48:21 +0300

    Business katmanına ait Abstract Servisler tanımlandı. IGenericService e ait metotlar tanımlanarak her entity için IGenericService den miras alınma işlemi yapıldı.

---
+++ SignalRProject/SignalR.BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.BusinessLayer.Abstract
+{
+    public interface IGenericService<T> where T : class
+    {
+        //Başına T gelme sebebi IGenericDal ile karışmaması için. Başında T olan metotlar business katmanındaki metotlar olacak, başında T olmayan metotlar ise DataAccess katmanındaki metotlar olacak. 
+
+        void TAdd(T entity); //Ekleme
+        void TDelete(T entity); //Silme 
+        void TUpdate(T entity); //Güncelleme
+        T TGetByID(int id); //Id ye göre Listeleme
+        List<T> TGetListAll(); //Tüm hepsini listeleme
+    }
+}
